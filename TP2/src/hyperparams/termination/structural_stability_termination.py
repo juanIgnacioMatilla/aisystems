dd6cc24b361a9a35c06954044fbd649644af6159
fpanighini <fpanighini@itba.edu.ar>
2024-09-06 19:37:55 -0300

    Added all termination methods required

---
+++ TP2/src/hyperparams/termination/structural_stability_termination.py
@@ -0,0 +1,33 @@
+from typing import List
+
+from TP2.src.hyperparams.termination.abstract_termination import Termination
+from TP2.src.model.individual import Individual
+
+class StructuralStabilityTermination(Termination):
+    def __init__(self, threshold: float, stability_generations: int):
+        self.threshold = threshold  # Proportion of population that must remain unchanged
+        self.stability_generations = stability_generations
+        self.stable_generations = 0
+        self.last_population_snapshot = None
+
+    def reset(self):
+        self.stable_generations = 0
+        self.last_population_snapshot = None
+
+    def should_terminate(self, population: List[Individual], generation: int) -> bool:
+        if self.last_population_snapshot is None:
+            self.last_population_snapshot = population.copy()
+
+        unchanged_individuals = sum(
+            1 for i, ind in enumerate(population)
+            if ind.chromosome == self.last_population_snapshot[i].chromosome
+        )
+
+        if unchanged_individuals / len(population) >= self.threshold:
+            self.stable_generations += 1
+        else:
+            self.stable_generations = 0
+
+        self.last_population_snapshot = population.copy()
+
+        return self.stable_generations >= self.stability_generations
--- TP2/utils/hyperparams_mapping.py
+++ TP2/utils/hyperparams_mapping.py
@@ -3,6 +3,9 @@ from TP2.src.hyperparams.mutation.single_gene_mutation import SingleGeneMutation
 from TP2.src.hyperparams.replacement.fill_all_replacement import FillAllReplacement
 from TP2.src.hyperparams.selection.elite_selection import EliteSelection
 from TP2.src.hyperparams.termination.gen_amount_termination import GenAmountTermination
+from TP2.src.hyperparams.termination.structural_stability_termination import StructuralStabilityTermination
+from TP2.src.hyperparams.termination.content_stability_termination import ContentStabilityTermination
+from TP2.src.hyperparams.termination.acceptable_solution_termination import AcceptableSolutionTermination
 
 SELECTION_MAP = {
     "elite": EliteSelection,
@@ -22,4 +25,7 @@ MUTATION_MAP = {
 
 TERMINATION_MAP = {
     "generation_amount": GenAmountTermination,
+    "structural_stability": StructuralStabilityTermination,
+    "content_stability": ContentStabilityTermination,
+    "acceptable_solution": AcceptableSolutionTermination,
 }
