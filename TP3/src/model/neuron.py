a304c5a60db0cccabd725f3b24a28df956beae8b
juanIgnacioMatilla <juanignaciomatilla@gmail.com>
2024-09-28 16:16:56 -0300

    deleted comment

--- TP3/src/model/neuron.py
+++ TP3/src/model/neuron.py
@@ -5,12 +5,10 @@ import numpy as np
 
 class Neuron:
     def __init__(self, weights: np.ndarray, activation_function: Callable):
-        # Inicializa los pesos aleatorios y un umbral (bias)
         self.weights = weights
         self.activation_function = activation_function
 
     def predict(self, inputs):
-        # Calcula la salida de la neurona
         inputs_with_bias = np.append(inputs, 1)
         linear_combination = np.dot(inputs_with_bias, self.weights)
         return self.activation_function(linear_combination)
